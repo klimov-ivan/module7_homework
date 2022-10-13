62efac52239a7039d4f7d5080dfea40f61d0d5c4
klimov-ivan <klimov.i.b@gmail.com>
2022-10-13 18:15:50 +0500

    7.3 Прототип и конструктор объекта

---
+++ module7_homework-7.3.js
@@ -0,0 +1,40 @@
+//Задание 1.
+
+const user = {
+	имя: 'Ivan',
+	фамилия: 'Klimov'
+}; 
+
+function db(usr) {
+	for (let key in usr) {
+
+		console.log( `${key}: ${usr[key]}`); 
+	 
+	};
+};
+db(user);
+
+//Задание 2.
+
+function check(str, obj) {
+	
+	console.log(str in obj);
+	
+};
+check('фамилия', user);
+
+//Задание 3.
+
+function createObj(obj) {
+	let staples = {};
+	
+	return obj = staples;
+  }
+
+  let object = createObj('obj')
+  console.log((object));
+
+
+
+
+
